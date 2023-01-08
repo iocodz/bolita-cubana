065854b850eb79d8d80813ceb392b8c73efb8d23
Raul C. Rivero <rahulrcr98@gmail.com>
2023-01-08 14:47:36 -0500

    rename file

--- utils/clean-result.js
+++ utils/clean-result.js
@@ -1,4 +1,4 @@
-module.exports = function clearResult(result) {
+module.exports = function cleanResult(result) {
   let r = result;
   const properties = ["_id", "comments", "rating"];
   properties.map((property) => delete r[property]);
